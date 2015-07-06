b0faa6f1fed3fda3d7465c4cd948cf5ac652a353
Desmond Lee <desmondclee@gmail.com>
2015-07-06 02:14:39 -0500

    build

---
+++ content/Blog.js
@@ -0,0 +1,4 @@
+
+// gulp generated file
+export default JSON.parse('[{"slug":"experimented-polymer","title":"Experimented with Polymer","date":"2014-11-17T00:00:00.000Z","component":"ContentPage","tags":["polymer","dev"]},{"slug":"flux-static-blog","title":"Static blog using React and Flux","date":"2014-11-22T00:00:00.000Z","component":"ContentPage","tags":["react","dev"]},{"slug":"getting-started-with-google-calendar-api","title":"Getting started with Google Calendar API","date":"2014-11-24T00:00:00.000Z","component":"ContentPage","tags":["apis","dev"]},{"slug":"react-and-firebase","title":"Learning how to use ReactJS and Firebase","date":"2014-11-6","component":"ContentPage","tags":["react","firebase","dev"]}]');
+
